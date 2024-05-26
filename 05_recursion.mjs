fd3b68b0a6c18e080c467d2fd60d72ed8b88d953
Igor Storchevoy <hellsgor@gmail.com>
2024-05-27 01:08:16 +0400

    studied recursion

---
+++ 05_recursion.mjs
@@ -0,0 +1,19 @@
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(3));
+
+function fibonachi(n) {
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+
+  return fibonachi(n - 1) + fibonachi(n - 2);
+}
+
+console.log(fibonachi(5));
